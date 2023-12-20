5f162614a01d96518a444f0765155000720bbf58
INDRAN Jayatharan <jayatharan@INDRANs-MacBook-Air.local>
2023-12-20 11:50:36 +0100

    Initial commit

---
+++ src/app.controller.ts
@@ -0,0 +1,26 @@
+import { Controller, Get, Sse } from '@nestjs/common';
+import { AppService } from './app.service';
+import { Observable, interval, map } from 'rxjs';
+
+interface MessageEvent {
+  data: string | object;
+}
+
+@Controller('api')
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Sse('event')
+  sendEvent(): Observable<MessageEvent> {
+    return interval(1000).pipe(
+      map((num: number) => ({
+        data: `Hello ${num}`,
+      })),
+    );
+  }
+}
